d3539fb4e142249564f83a801d397b0a6dc5038b
stulzq <stulzq@qq.com>
2018-08-01 16:07:19 +0800

    修改：回调地址取当前域名

--- src/Alipay.Demo.PCPayment/Controllers/PayController.cs
+++ src/Alipay.Demo.PCPayment/Controllers/PayController.cs
@@ -25,34 +25,6 @@ namespace Alipay.Demo.PCPayment.Controllers
 		    return View();
 	    }
 
-	    public IActionResult Test()
-	    {
-			DefaultAopClient client = new DefaultAopClient("https://openapi.alipaydev.com/gateway.do", "2016090800466366", "MIIEogIBAAKCAQEAuCcfh+EJHfMwzXC6b8HfzjYDcH7y79dKAdh5Y5rXOO/4y9stKke+F4Pdp7BOzu6N8/jse7xxCiVLdyUO0L/ck5hOWna//fdQPcSbEyYBUQn+CQy7+uDbsZRniXeeaHJBnK5fot3oHzpHe4dScnvG5NKRJAYsaQW5vD8cVc/m31yuocbyPgqUllblK3h8Lg/nh64bKl8e0Hck1EgGm7+W0+IojM9rcMY1BrdIlUiuP25u8I3zPFH5Tjk4otCvm+xDsukeJiftmDrhtmW3cGtTpNFtr6DK+acfNVccyiaGeCidvLcT3ziO8V7Cy1oQVk+8j5qg83xzCslvF0kZwwrQXwIDAQABAoIBAErfbUx7zI8mz5LX4JWLyAk5oNBebTvi1q5Fa6V50UPPHeMUrBpirQE2liqV3pT4HTMy5EOy7GrpmvNIV+u79lz0MfdKDOmfYjqa80ony6U9YNIhIVTxk6Fx617TCc8BlXe01iGMA9KeiW3KKVVuygFDM8vnzqqsr0XAiy7ApuftP4ujFCJhDZ/PSaKB1/YNJQjz/2GRjqbYT5irre3Mjz7FgyN19CJmSnjshdixp0ItOTQ7QsHYE537gGZhEXs+RDFopF+w4y45kfUUh1IpBxD7jlD1zO3OHXJxBqSiJxz4iO8FS/OYdQUMoGpTtJc6bMnmZcLSwnfV9RTIB381BoECgYEA7+AcpfIpKWgc1zOJRKTjBdKe77LTjto30UlyeskpUofKVRqEYTgALL7POlH16L9gA2R8HvsSEmAPGw7P4ptYO+Kfi3TeiG6RwT5K+JSB8g0xqwvDEVOD3May+nfnaz5sMktWNirSFHbbymfh99mj+4E1drv3cOuDJR6v2qicTyECgYEAxIgcUkcG6CpHagxYfaboyvmglbq/9ZD+ruZSvqG877NT0NLqEPZ6ZOd6AS9m/GsC6pUABtFfXK256oZ7H8bqsfIFTfoUbjiy3fXNuniuvqtLTGL+2kyCazhV15kLtX24wCV6M32Yc1DvGPywAI40UG9D6D2MTCuu7KgO715or38CgYA9p8AVU0oLL4yCL+fvceY8X+ekOrWv+RzxuUwojT4GzYpPF5LBHlDFL4I6PkjTuyTlmlVg7S229WPMk6ERYNZsBhL2GGL+dFUYc3d3r0w7N/L1QP+xm2LAQ35LbLhZ02CiCMUvBRCcW/SgcNUfDJzej1Z7n1K5fn9l8h5HOKF8oQKBgBP7Hp4C2KHsAny2qpyDxrE0Ne5jITcPOcWAZzM4cGQSYFgfyWpWFNWDbzUFo7vQCWjeIzWOPdrHUtqUN5pgd+YFjCKEZWVbYFwOrI7jzChYc/xdKDn7g1rxTFDyH22hTZJDfSwU/dXjiZuJvT8hNhJjbY0EDOqFmnA3GeWePJhzAoGAXioZrM79oG/iN59XuAA5Arl5UjkCsKPa1rfALCSfEoJfvH1UAQ1SbuM0MMIAOGJc/yPdDjyv8Kdy7z867gxo6m0vuLdyJ6qL/KI//mNLJpW30I6/pYsWO1LIIxusxTkv459mvGufn1WH0LHpAf4b84/uq2EDszVat7MEt48NlSI=", "json", "2.0",
-			    "RSA2", "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyWWqIIJ0KKDUPk6fgbxXLxCDqVWTvV4mplocxRsjFPvZC2e4uHs6hq3n8/bc/ZnLcavDTCx56L+TsTKN8w4984pqYPgkYXjWl26hMvVJ2GizjRGw+FeM+1uqiWzrDflX3Cp/o8HKSeFqjA+HXpX+nFg94XJfYIAf8vtHKCusdJE6YRmUZ7lCYH01M59b3KQBeX2q304GwC27xgZ1ZOuqGdngNeeTo0WgTpfKOzgQkUVDNLjVJhxk6i71vzfJ5QUUVIEff1UNlLK1C/kCGDaSY8DGPXni+BfdQekVxdqO1aV5JvaZVW9KrxnXfyAf0FYelBbqym4qzJu98+8eee/oyQIDAQAB", "UTF-8", false);
-
-		    AlipayOpenAuthTokenAppModel model = new AlipayOpenAuthTokenAppModel();
-
-		    //如果使用app_auth_code换取token，则为authorization_code，如果使用refresh_token换取新的token，则为refresh_token
-		    model.GrantType = "authorization_code";
-
-		    //与refresh_token二选一，用户对应用授权后得到，即第一步中开发者获取到的app_auth_code值
-		    model.Code = "ab64829e4d5b4e2aabd2ab6bae0dcB72";
-
-		    //与code二选一，可为空，刷新令牌时使用
-		    model.RefreshToken = "";
-
-		    AlipayOpenAuthTokenAppRequest request = new AlipayOpenAuthTokenAppRequest();
-
-		    request.SetBizModel(model);
-
-		    request.SetReturnUrl("");
-
-		    var response = client.Execute(request);
-
-		    return new JsonResult(new { data = response });
-
-		}
-
 		/// <summary>
 		/// 发起支付请求
 		/// </summary>
@@ -64,9 +36,6 @@ namespace Alipay.Demo.PCPayment.Controllers
 		[HttpPost]
 	    public void PayRequest(string tradeno,string subject,string totalAmout,string itemBody)
 	    {
-//		    DefaultAopClient client = new DefaultAopClient(Config.Gatewayurl, Config.AppId, Config.PrivateKey, "json", "1.0",
-//			    Config.SignType, Config.AlipayPublicKey, Config.CharSet, false);
-
 		    // 组装业务参数model
 		    AlipayTradePagePayModel model = new AlipayTradePagePayModel
 		    {
@@ -79,7 +48,7 @@ namespace Alipay.Demo.PCPayment.Controllers
 
 		    AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();
 		    // 设置同步回调地址
-		    request.SetReturnUrl("http://localhost:5000/Pay/Callback");
+		    request.SetReturnUrl($"http://{Request.Host}/Pay/Callback");
 		    // 设置异步通知接收地址
 		    request.SetNotifyUrl("");
 		    // 将业务model载入到request
